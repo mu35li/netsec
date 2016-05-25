ca7f2cdda1f2d1c7e5906ab10452ecc87388c79e
Julian Deinert <j.deinert@gmx.net>
2016-05-25 18:24:58 +0200

    Add register login and bruteforce feature to chat

--- labreport03/javashit/ClientWorker.java
+++ labreport03/javashit/ClientWorker.java
@@ -11,6 +11,8 @@ class ClientWorker implements Runnable {
   private Socket client;
   private JTextArea textArea;
   private SocketThrdServer server;
+  private boolean isLoggedIn = false;
+  private int failedAttempts = 0;
   ClientWorker(Socket client, JTextArea textArea, SocketThrdServer server) {
    this.client = client;
    this.textArea = textArea;
@@ -31,12 +33,47 @@ class ClientWorker implements Runnable {
   while(true){
     try{
       line = in.readLine();
+      System.out.println(line);
+      if (line.equals("register")) {
+        out.println("Please enter a username");
+        String username = in.readLine();
+        out.println("Please enter a password");
+        String password = in.readLine();
+        textArea.append("User wants to register with username: "+username+" and password "+password+"\n");
+        boolean register = this.register(username, password);
+        if (register == true) {
+          out.println("registered successfully");
+        }
+      }else if(line.equals("login")){
+        if (failedAttempts >=3) {
+          out.println("Too many failed attempts");
+        }else{
+          out.println("Please enter a username");
+          String username = in.readLine();
+          out.println("Please enter a password");
+          String password = in.readLine();
+          textArea.append("User wants to login with username: "+username+" and password "+password+"\n");
+          boolean login = this.login(username, password);
+          if (login == true) {
+            out.println("logged in successfully");
+          }else{
+            this.failedAttempts ++;
+            if (failedAttempts >=3) {
+              out.println("Login has been blocked");
+            }
+            out.println("login failed");
+          }
+        }
+      }else{
 //Send data back to client
-      if (line != null) {
+      if (line != null && this.isLoggedIn) {
        out.println(line);
        textArea.append(line+"\n");
        server.broadCast(this, line);
+     }else{
+      out.println("Please login to chat");
      }
+   }
    } catch (IOException e) {
      System.out.println("Read failed");
      System.exit(-1);
@@ -57,6 +94,21 @@ protected void finalize(){
     //Objects created in run method are finalized when 
     //program terminates and thread exits
   server.unlistenWorker(this);
+}
+private boolean register(String username, String password){
+  Useradmin useradminObj = new Useradmin();
+  useradminObj.addUser(username, password);
+  return true;
+}
+private boolean login(String username, String password){
+  Useradmin useradminObj = new Useradmin();
+  boolean check = useradminObj.checkUser(username, password);
+  if (check == true) {
+    this.isLoggedIn = true;
+    return true;
+  }else{
+    return false;
+  }
 }
 }
 
---
+++ labreport03/javashit/Useradministration.java
@@ -0,0 +1,12 @@
+
+/**
+ * Write a description of interface Useradministration here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public interface Useradministration
+{
+    public void addUser(String username, String password);
+    public boolean checkUser(String username, String password);
+}
