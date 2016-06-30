6ddb84bf398f8d6a5983b67a66a3f1301b9ccbe0
tronje <hi@tron.je>
2016-06-30 15:56:58 +0200

    06: add a bunch of material, as well as bazi crack script

---
+++ labreport06/crack_bazi.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import sys, os
+
+PATH='/mnt/baziCrypt/'
+
+if __name__ == "__main__":
+    filename = sys.argv[1]
+    result = ''
+    counter = -1
+    with open(os.path.join(PATH, filename), "rb") as f:
+        f.seek(-10, 2) # seek last 10 bytes
+        key = [int(b) for b in f.read(10)]
+        f.seek(0, 0) # seek beginning of file again
+        while True:
+            counter += 1
+            counter = counter % 10
+            b = f.read(1)
+            if b == b'':
+                break
+            num = int.from_bytes(b, 'little')
+            result += chr(num ^ key[counter])
+    print(result)
