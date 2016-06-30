30330d08d8f089083ca68f626cc703613c2dd10f
tronje <hi@tron.je>
2016-06-30 17:19:49 +0200

    06: cock piss fuck shit

---
+++ labreport06/crack_advazi.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import sys
+
+if __name__ == "__main__":
+    filename = sys.argv[1]
+    result = ''
+    counter = -1
+    with open(filename, "rb") as f:
+        f.seek(-10, 2) # seek last 10 bytes
+        key = [int(b) for b in f.read(10)]
+        assert len(key) == 10
+        for i in range(len(key)):
+            key[i] ^= 0x0a
+        f.seek(0, 0) # seek beginning of file again
+        while True:
+            counter += 1
+            counter = counter % 10
+            b = f.read(1)
+            if b == b'':
+                break
+            num = int.from_bytes(b, 'little')
+            result += chr(num ^ key[counter])
+    print(result)
