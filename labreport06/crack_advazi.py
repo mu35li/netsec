2890c9e845e302ea97cfe91fa04674395a74e32c
tronje <hi@tron.je>
2016-06-30 21:06:28 +0200

    06: clean up scripts and make them a little more 'professional'

--- labreport06/crack_advazi.py
+++ labreport06/crack_advazi.py
@@ -1,34 +1,48 @@
 #!/usr/bin/env python
-import sys
+def crack(filename, bsize):
+    """Decrypt an advazi-encrypted file given the file's
+    name and the blocksize (usually 10)
+    """
 
-if __name__ == "__main__":
-    filename = sys.argv[1]
     result = ''
-    counter = -1
     with open(filename, "rb") as f:
-        f.seek(-10, 2) # seek last 10 bytes
-        key = [int(b) for b in f.read(10)]
-        assert len(key) == 10
-        f.seek(0, 0) # seek beginning of file again
-        whole_file = f.read()
+        f.seek(-bsize, 2) # seek last n bytes
 
+        # initialize key as last n bytes
+        key = [int(b) for b in f.read(bsize)]
+
+        f.seek(0, 0) # seek beginning of file again
+        whole_file = f.read() # read in the entire file
+
+        # compute padding
         padding = 0
+        counter = bsize - 1
         for i in range(len(whole_file) - 1, 0, -1):
-            if whole_file[i] not in key:
+            if whole_file[i] != key[counter]:
                 break
             else:
                 padding += 1
+            counter -= 1
+            counter %= 10
 
+        # decrypt key with padding value
         for i in range(len(key)):
             key[i] ^= padding
 
+        counter = -1
         f.seek(0, 0) # seek beginning of file again
+
+        # decrypt plaintext
         while True:
             counter += 1
-            counter = counter % 10
+            counter %= 10
             b = f.read(1)
             if b == b'':
                 break
             num = int.from_bytes(b, 'little')
             result += chr(num ^ key[counter])
-    print(result[:len(result) - padding])
+    return result[:len(result) - padding]
+
+if __name__ == "__main__":
+    import sys
+    print(crack(sys.argv[1], 10))
--- labreport06/crack_bazi.py
+++ labreport06/crack_bazi.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python
-import sys
+def crack(filename, bsize):
+    """Decrypt a bazi-encrypted file given the file's
+    name and the blocksize (usually 10)
+    """
 
-if __name__ == "__main__":
-    filename = sys.argv[1]
     result = ''
     counter = -1
     with open(filename, "rb") as f:
-        f.seek(-10, 2) # seek last 10 bytes
-        key = [int(b) for b in f.read(10)]
+        f.seek(-bsize, 2) # seek last n bytes
+        key = [int(b) for b in f.read(bsize)]
         f.seek(0, 0) # seek beginning of file again
         while True:
             counter += 1
-            counter = counter % 10
+            counter = counter % bsize
             b = f.read(1)
             if b == b'':
                 break
             num = int.from_bytes(b, 'little')
             result += chr(num ^ key[counter])
-    print(result)
+    return result
+
+
+if __name__ == "__main__":
+    import sys
+    print(crack(sys.argv[1], 10))
